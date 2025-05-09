0c7eab1e3149ba346765be018d3c6005de6d8127
Yamatchi <ussousso12@gmail.com>
2025-05-09 12:58:52 +0900

    feat : Animation image PPU 수정
    Animation image PPU 수정
    100 -> 32 수정

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -5,28 +5,58 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private float baseJump = 5f;
 
     Rigidbody2D rb;
     Animator anim;
+
+    bool isJump;
+
+    Vector2 movementVelo;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
     private void FixedUpdate()
     {
         Move();
+        Run();
+        Jump();
     }
 
     private void Move()
     {
+        // 본 게임은 Horizontal, Vertical이 없음 
+        // Vertical 자동 이동임
         float moveh = Input.GetAxisRaw("Horizontal");
         float movev = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveh, movev);
         rb.velocity = movement * baseSpeed;
-        Vector2 movementVelo = rb.velocity;
+        movementVelo = rb.velocity;
 
+    }
+    void Run()
+    {
+        if(movementVelo != Vector2.zero)
+        {
+            anim.SetBool("isRun", true);
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            Debug.Log("Jump");
+            rb.AddForce(Vector2.up * baseJump, ForceMode2D.Impulse);
 
+            isJump = true;
+            anim.SetBool("isJump", true);
+        }
     }
 }
