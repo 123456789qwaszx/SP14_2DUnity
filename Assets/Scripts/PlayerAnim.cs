cedf4bb362bf86d3c376d347d1f36b315ec0ddfb
Yamatchi <ussousso12@gmail.com>
2025-05-09 11:56:51 +0900

    test : 테스트용 이동 로직 추가
    테스트용 이동 로직 추가

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -2,15 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerAnim : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float baseSpeed = 5f;
+
+    Rigidbody2D rb;
+    Animator anim;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
     }
 
-    void Update()
+    private void Move()
     {
-        
+        float moveh = Input.GetAxisRaw("Horizontal");
+        float movev = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveh, movev);
+        rb.velocity = movement * baseSpeed;
+        Vector2 movementVelo = rb.velocity;
+
+
     }
 }
