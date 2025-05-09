c24321e91aa440ddb2087313f334cfbda87b2a24
Yamatchi <ussousso12@gmail.com>
2025-05-09 15:02:51 +0900

    feat : Animator 수정
    Animator 수정

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -5,58 +5,75 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     [SerializeField] private float baseSpeed = 5f;
-    [SerializeField] private float baseJump = 5f;
+    [SerializeField] private float baseJump = 30f;
 
     Rigidbody2D rb;
     Animator anim;
 
     bool isJump;
+    bool jDown;
+
+    float hAxis;
 
     Vector2 movementVelo;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+
+        
     }
     private void FixedUpdate()
     {
+        GetInput();
         Move();
-        Run();
         Jump();
     }
 
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        jDown = Input.GetButtonDown("Jump");
+    }
     private void Move()
     {
-        // 본 게임은 Horizontal, Vertical이 없음 
-        // Vertical 자동 이동임
-        float moveh = Input.GetAxisRaw("Horizontal");
-        float movev = Input.GetAxisRaw("Vertical");
-
-        Vector2 movement = new Vector2(moveh, movev);
+        Vector3 movement = new Vector3(hAxis, 0);
         rb.velocity = movement * baseSpeed;
         movementVelo = rb.velocity;
 
+        anim.SetBool("isRun", movementVelo != Vector2.zero);
     }
-    void Run()
-    {
-        if(movementVelo != Vector2.zero)
-        {
-            anim.SetBool("isRun", true);
-        }
-        else
-        {
-            anim.SetBool("isRun", false);
-        }
-    }
+    //void Run()
+    //{ 
+    //    if(movementVelo != Vector2.zero)
+    //    {
+    //        anim.SetBool("isRun", true);
+    //    }
+    //    else
+    //    {
+    //        anim.SetBool("isRun", false);
+    //    }
+    //} 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && !isJump)
+        if (jDown && !isJump)
         {
             Debug.Log("Jump");
-            rb.AddForce(Vector2.up * baseJump, ForceMode2D.Impulse);
+            rb.AddForce(Vector3.up * baseJump, ForceMode2D.Impulse);
 
-            isJump = true;
             anim.SetBool("isJump", true);
+            isJump = true;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+
         }
     }
 }
