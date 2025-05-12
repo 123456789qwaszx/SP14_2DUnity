ccb43f9a9e8df10a7c54f8fe1da752b38de88ac9
ChaWooJin <chawoojin06@gmail.com>
2025-05-12 13:01:54 +0900

    faet_Update

--- Assets/Scripts/Controller/Character/CharacterBaseController.cs
+++ Assets/Scripts/Controller/Character/CharacterBaseController.cs
@@ -23,8 +23,8 @@ public class CharacterBaseController : MonoBehaviour
     protected bool isSliding = false;
     protected bool isGround = false;
     public float CurrentHp { get; set; }
-    public float CurrentSpeed { get; set; }
-    public float CurrentJumpPower { get; set; }
+    public float CurrentSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
+    public float CurrentJumpPower { get { return jumpPower; } set { jumpPower = value; } }
 
     [Header("Character Interaction")]
     private Vector2 knockBack = Vector2.zero;   // 장애물에 부딪힌 이후 캐릭터가 밀려나는 힘
--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -10,26 +10,24 @@ public class Items : MonoBehaviour
 
     [SerializeField] private SpriteRenderer _scaleUp;
     [SerializeField] private SpriteRenderer _heathREcovery;
+    [SerializeField] private SpriteRenderer _speedUp;
 
-    bool posstop = true;
+    private float maxSpeed = 5f;
+    private float duration = 2f;
 
-    float originSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // 플레이어 찾기
-        _player = GetComponent<CharacterController>();
-    }
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     private void HeathRecovery()
     {
         // 플레이어 hp받아서 증가
         if (_player.CurrentHp > 0 && _player.CurrentHp < 3)
         {
             _player.CurrentHp += 1;
+            Destroy(gameObject);
         }
         else
         {
+            Destroy(gameObject);
             return;
         }
     }
@@ -41,30 +39,52 @@ public class Items : MonoBehaviour
     {
         StartCoroutine(ScaleUpCoroutine(_duration));
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     private IEnumerator ScaleUpCoroutine(float _duration)
     {
         Vector3 originalScale = _player.transform.localScale;
 
         _player.transform.localScale = originalScale + new Vector3(1.0f, 1.0f, 0f);
 
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
         yield return new WaitForSeconds(_duration);
 
         _player.transform.localScale = originalScale;
+
+        Destroy(gameObject);
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     private IEnumerator SpeedUpCoroutine(float _speedUp, float _duration)
     {
+        // x 값 속도 상승
         _player.CurrentSpeed = 5f;
-        originSpeed = _player.CurrentSpeed;
+        float originSpeed = _player.CurrentSpeed;
 
         _player.CurrentSpeed += _speedUp;
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
 
         yield return new WaitForSeconds(_duration);
 
         _player.CurrentSpeed = originSpeed;
+
+        Destroy(gameObject);
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        posstop = false;
+        _player = collision.gameObject.GetComponent<CharacterController>();
+
+        if (_player == null) return;
 
         if (collision.gameObject.CompareTag("HeathRecovery"))
         {
@@ -72,13 +92,12 @@ public class Items : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("ScaleUp"))
         {
-            ScaleUp(3);
+            ScaleUp(duration);
         }
-        else if (collision.gameObject.CompareTag("ScaleUp"))
+        else if (collision.gameObject.CompareTag("SpeedUp"))
         {
-            SpeedUp(5, 3);
+            SpeedUp(maxSpeed, duration);
         }
-
         Debug.Log("충돌");
     }
 }
