7f5984e5333229555826675a7476489aeba5abb4
DESKTOP-FVBU6NK\Sejin <sjb0710@gmail.com>
2025-05-14 10:01:16 +0900

    refactor: 각 아이템별 스크립트 분리

---
+++ Assets/Scripts/GigantizeItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 플레이어의 크기를 증가시키는 아이템
+public class GigantizeItem : Items
+{
+    // memo: 슈퍼모드 부여 여부도 고려해볼 것
+    private float scaleUpDuration = 2f; // 사이즈업은 이 아이템의 고유변수이므로 이관
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CharacterController _player = collision.gameObject.GetComponent<CharacterController>();
+
+        if (_player == null) return;
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ScaleUp(_player, scaleUpDuration);
+        }
+    }
+}
