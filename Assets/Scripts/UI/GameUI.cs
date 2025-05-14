011f6bc2c7c7e2de09c095c42632da161c0034e2
jkh201020 <jack0517@naver.com>
2025-05-15 02:55:06 +0900

    docs: GameUI 주석 꺠진거 수정
     GameUI 주석 꺠진거 수정

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -10,37 +10,37 @@ using UnityEditor;
 
 public class GameUI : MonoBehaviour
 {
-    [Header("ĵ���� ������Ʈ")]
+    [Header("캔버스")]
     [SerializeField] private GameObject _gameUICanvas;
     [SerializeField] private GameObject _gameStateUICanvas;
 
-    [Header("��Ʈ �̹���")]
+    [Header("HP 이미지")]
     [SerializeField] private Sprite heart_full;
     [SerializeField] private Sprite heart_empty;
     [SerializeField] private List<Image> hearts;
 
-    [Header("���� ���� ����")]
+    [Header("게임 상태 UI 문구")]
     [SerializeField] private TextMeshProUGUI _gameStateText; // 게임 상태 문구
     [SerializeField] private string[] _gameStateMessages; // 게임 상태 문구 배열
 
     private CharacterController character;
 
-    private TextMeshProUGUI currentScoreTxt; // ���� ���� 
-    private TextMeshProUGUI bestScoreTxt; // �ְ� ����
-    private TextMeshProUGUI stateCurrentScoreTxt; // ���� ���� ���� ���� 
-    private TextMeshProUGUI stateBestScoreTxt; // ���� ���� �ְ� ����
+    private TextMeshProUGUI currentScoreTxt; // 인게임 UI 점수
+    private TextMeshProUGUI bestScoreTxt; // 인게임 UI 최고 점수
+    private TextMeshProUGUI stateCurrentScoreTxt; // 게임 종료 UI에서 사용하는 점수
+    private TextMeshProUGUI stateBestScoreTxt; // 게임 종료 UI에서 사용하는 최고 점수
 
-    private Button jumpButton; // ���� ��ư
-    private Button restartButton; // ����� ��ư
-    private Button backButton; // �ڷΰ��� ��ư
-    private Button homeButton; // Ȩ���� ���� ��ư
-    private Button pauseButton; // �Ͻ����� ��ư
-    private Button slidingButton; // �����̵� ��ư
+    private Button jumpButton; // 점프 버튼
+    private Button restartButton; // 재시작 버튼
+    private Button backButton; // 뒤로가기 버튼
+    private Button homeButton; // 메인 화면 가기 버튼
+    private Button pauseButton; // 일시정지 버튼
+    private Button slidingButton; // 슬라이딩 버튼
 
     public GameObject[] charPrefabs;
     public GameObject player;
 
-    public bool HpUp = false; // ȸ���ߴ��� ����
+    public bool HpUp = false; // 체력을 회복한 상태인지 확인
 
     private void Start()
     {
@@ -57,7 +57,7 @@ public class GameUI : MonoBehaviour
 
     public void Init() // �ʱ�ȭ
     {
-        // �ν����Ϳ� ������Ʈ ����
+        // 다음부터는 이렇게 안 하고 컴포넌트에 할당하겠습니다.
         Transform gameCanvas = _gameUICanvas.transform;
         Transform gameStateCanvas = _gameStateUICanvas.transform;
 
@@ -84,45 +84,45 @@ public class GameUI : MonoBehaviour
 
         character = playerObject.GetComponent<CharacterController>();
         _gameUICanvas.SetActive(true);
-        character.SetCharacterState(); // ĳ���� ���� �ʱ�ȭ
-        LoadBestScore(); // �ҷ��� �ְ� ���� ǥ��
+        character.SetCharacterState(); // 캐릭터 상태 초기화
+        LoadBestScore(); // 최고 점수 불러오기
 
-        Time.timeScale = 1.0f; // ���ӽ���
+        Time.timeScale = 1.0f; // 게임 시작
     }
 
-    public void SetUI(int currentscore, int bestscore) // ������ �޾ƿ�
-    {
+    public void SetUI(int currentscore, int bestscore) // 받아 온 점수를 UI에 연결
+    { 
         currentScoreTxt.text = currentscore.ToString();
         bestScoreTxt.text = bestscore.ToString();
         stateCurrentScoreTxt.text = currentscore.ToString();
         stateBestScoreTxt.text = bestscore.ToString();
     }
 
-    public void SaveBestScore()
+    public void SaveBestScore() // 최고 점수 저장
     {
         PlayerPrefs.SetInt("BestScore", character.BestScore);
-        PlayerPrefs.Save(); // ���� ������ ��ũ�� ����
+        PlayerPrefs.Save();
     }
 
-    public void LoadBestScore() // �ְ� ���� ����
+    public void LoadBestScore() // 게임 시작 시 최고 점수 불러오기
     {
-        if (PlayerPrefs.HasKey("BestScore")) // �ش� Ű�� �����ϴ��� Ȯ��
+        if (PlayerPrefs.HasKey("BestScore"))
         {
             character.BestScore = PlayerPrefs.GetInt("BestScore");
             bestScoreTxt.text = character.BestScore.ToString();
         }
         else
         {
-            character.BestScore = 0; // ����� �ְ� ������ ������ 0���� �ʱ�ȭ
+            character.BestScore = 0; // 최고 점수가 없을 경우 초기화
             bestScoreTxt.text = "0";
         }
     }
 
-    private void ShowGameStateUI() // ���ӻ��� UI
+    private void ShowGameStateUI() // 게임 상태 UI 보여주기
     {
-        if (character.CurrentHp <= 0) // 체ㄹ
+        if (character.CurrentHp <= 0) // 현재 체력이 0일 경우
         {
-            _gameStateText.text = _gameStateMessages[1]; // "���� ����" ���
+            _gameStateText.text = _gameStateMessages[1]; // GameOver 출력
             _gameStateUICanvas.SetActive(true);
             Time.timeScale = 0f;
         }
@@ -177,14 +177,14 @@ public class GameUI : MonoBehaviour
         SetUI(character.Score, character.BestScore);
     }
 
-    public void CheckGameOver() // ���� ������ ��� ����
+    public void CheckGameOver() // 게임 오버 시 
     {
         StartCoroutine(DelayedGameOverUI());
     }
 
     IEnumerator DelayedGameOverUI()
     {
-        yield return new WaitForSeconds(0.5f); // 0.5�� ��� �� �ٽ� �۵�
+        yield return new WaitForSeconds(0.5f); // 0.5초 후 게임 종료 및 UI 출력
         Debug.Log("0.5�� �Ŀ� UI�۵�");
         ShowGameStateUI();
     }
