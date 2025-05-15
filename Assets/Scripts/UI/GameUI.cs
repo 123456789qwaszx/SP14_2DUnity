d39807667d044dbdcf96932e35270dae73257f24
jkh201020 <jack0517@naver.com>
2025-05-15 10:10:06 +0900

    docs: AddListener코드 수정
    작업: GameUI.cs, GameUI 프리팹
    
    내용: AddListener제거하고 Button컴포넌트에 직접 할당하는 방식으로 수정.
    클리어 시 UI 오른쪽 하단 버튼을 이어하기 버튼으로 바꿔서 무한 모드처럼 할 수 있습니다.
    일시정지 했을 때는 이어서 진행하도록 수정했습니다.
    죽었을 경우에는 스테이지 선택 창으로 이동하도록 수정했습니다.
    오류는 딱히 없고 재시작도 잘 작동하는 것 같습니다.
    GameUI스크립트도 수정해서 로딩도 줄었습니다.

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -14,6 +14,12 @@ public class GameUI : MonoBehaviour
     [SerializeField] private GameObject _gameUICanvas;
     [SerializeField] private GameObject _gameStateUICanvas;
 
+    [Header("점수 문구 연결")]
+    [SerializeField] private TextMeshProUGUI currentScoreTxt; // 인게임 UI 점수
+    [SerializeField] private TextMeshProUGUI bestScoreTxt; // 인게임 UI 최고 점수
+    [SerializeField] private TextMeshProUGUI stateCurrentScoreTxt; // 게임 종료 UI에서 사용하는 점수
+    [SerializeField] private TextMeshProUGUI stateBestScoreTxt; // 게임 종료 UI에서 사용하는 최고 점수
+
     [Header("HP 이미지")]
     [SerializeField] private Sprite heart_full;
     [SerializeField] private Sprite heart_empty;
@@ -24,23 +30,11 @@ public class GameUI : MonoBehaviour
     [SerializeField] private string[] _gameStateMessages; // 게임 상태 문구 배열
 
     private CharacterController character;
-
-    private TextMeshProUGUI currentScoreTxt; // 인게임 UI 점수
-    private TextMeshProUGUI bestScoreTxt; // 인게임 UI 최고 점수
-    private TextMeshProUGUI stateCurrentScoreTxt; // 게임 종료 UI에서 사용하는 점수
-    private TextMeshProUGUI stateBestScoreTxt; // 게임 종료 UI에서 사용하는 최고 점수
-
-    private Button jumpButton; // 점프 버튼
-    private Button restartButton; // 재시작 버튼
-    private Button backButton; // 뒤로가기 버튼
-    private Button homeButton; // 메인 화면 가기 버튼
-    private Button pauseButton; // 일시정지 버튼
-    private Button slidingButton; // 슬라이딩 버튼
-
     public GameObject[] charPrefabs;
     public GameObject player;
 
     public bool HpUp = false; // 체력을 회복한 상태인지 확인
+    public bool isClear = false; // 클리어 했는지 
 
     private void Start()
     {
@@ -55,31 +49,8 @@ public class GameUI : MonoBehaviour
         ShowGameStateUI();
     }
 
-    public void Init() // �ʱ�ȭ
+    public void Init() // 초기화
     {
-        // 다음부터는 이렇게 안 하고 컴포넌트에 할당하겠습니다.
-        Transform gameCanvas = _gameUICanvas.transform;
-        Transform gameStateCanvas = _gameStateUICanvas.transform;
-
-        currentScoreTxt = gameCanvas.Find("CurrentScoreText").GetComponent<TextMeshProUGUI>();
-        bestScoreTxt = gameCanvas.Find("BestScoreText").GetComponent<TextMeshProUGUI>();
-        stateCurrentScoreTxt = gameStateCanvas.Find("CurrentScoreText").GetComponent<TextMeshProUGUI>();
-        stateBestScoreTxt = gameStateCanvas.Find("BestScoreText").GetComponent<TextMeshProUGUI>();
-
-        jumpButton = gameCanvas.Find("Button - Jump").GetComponent<Button>();
-        slidingButton = gameCanvas.Find("Button - Sliding").GetComponent<Button>();
-        restartButton = gameStateCanvas.Find("Button - Restart").GetComponent<Button>();
-        homeButton = gameStateCanvas.Find("Button - Home").GetComponent<Button>();
-        backButton = gameStateCanvas.Find("Button - Next").GetComponent<Button>();
-        pauseButton = gameCanvas.Find("Button - Pause").GetComponent<Button>();
-
-        jumpButton.onClick.AddListener(OnClickJumpButton);
-        slidingButton.onClick.AddListener(OnClickSlidingButton);
-        restartButton.onClick.AddListener(OnClickRestartButton);
-        backButton.onClick.AddListener(OnClickNextButton);
-        homeButton.onClick.AddListener(OnClickHomeButton);
-        pauseButton.onClick.AddListener(OnClickPauseButton);
-
         GameObject playerObject = player;
 
         character = playerObject.GetComponent<CharacterController>();
@@ -91,7 +62,7 @@ public class GameUI : MonoBehaviour
     }
 
     public void SetUI(int currentscore, int bestscore) // 받아 온 점수를 UI에 연결
-    { 
+    {
         currentScoreTxt.text = currentscore.ToString();
         bestScoreTxt.text = bestscore.ToString();
         stateCurrentScoreTxt.text = currentscore.ToString();
@@ -126,11 +97,13 @@ public class GameUI : MonoBehaviour
             _gameStateUICanvas.SetActive(true);
             Time.timeScale = 0f;
         }
-        else if (character.Score >= 5000) // 스코어 5000점 이상이면 클리어 UI출력
+
+        if (character.Score >= 5000 && !isClear) // 스코어 5000점 이상이면 클리어 UI출력
         {
             _gameStateText.text = _gameStateMessages[0]; // 클리어 문구 출력
             _gameStateUICanvas.SetActive(true);
             Time.timeScale = 0f;
+            isClear = true;
         }
     }
 
@@ -185,13 +158,13 @@ public class GameUI : MonoBehaviour
     IEnumerator DelayedGameOverUI()
     {
         yield return new WaitForSeconds(0.5f); // 0.5초 후 게임 종료 및 UI 출력
-        Debug.Log("0.5�� �Ŀ� UI�۵�");
+        Debug.Log("0.5초 후 UI 출력");
         ShowGameStateUI();
     }
 
     #region ��ư
 
-    private void OnClickJumpButton() // ���� ��ư
+    public void OnClickJumpButton() // 점프 버튼
     {
         if (character.jumpCount < character.maxJumpCount && !character.isSliding)
         {
@@ -199,7 +172,7 @@ public class GameUI : MonoBehaviour
         }
     }
 
-    private void OnClickSlidingButton() // �����̵� ��ư
+    public void OnClickSlidingButton() // 슬라이딩 버튼
     {
         if (character.isSliding == false)
         {
@@ -213,28 +186,37 @@ public class GameUI : MonoBehaviour
         }
     }
 
-    private void OnClickRestartButton() // ����� ��ư
+    public void OnClickRestartButton() // 재시작 버튼
     {
-        Time.timeScale = 1f; // ���� ����
+        Time.timeScale = 1f; // 게임 시작
         SceneManager.LoadScene("Game");
-        Debug.Log("�����");
+        Debug.Log("재시작");
     }
 
-    private void OnClickNextButton() // ���� �������� ��ư
+    public void OnClickNextButton() // 이어하기 버튼
     {
-        Debug.Log("���� ��������");
+        if ((int)character.CurrentHp == 0) // 게임 오버면 이어하기가 아닌 뒤로가기로 변경
+        {
+            SceneManager.LoadScene("SelecScene");
+        }
+        else if (character.Score >= 5000 || (int)character.CurrentHp > 0)
+        {
+            _gameStateUICanvas.SetActive(false);
+            Time.timeScale = 1f; // 게임 이어서 시작
+            Debug.Log("이어하기");
+        }
     }
 
-    private void OnClickHomeButton() // Ȩ ��ư
+    public void OnClickHomeButton() // 메인으로 이동
     {
         SceneManager.LoadScene("Main");
     }
 
-    private void OnClickPauseButton() // �Ͻ����� ��ư
+    public void OnClickPauseButton() // 일시 정지 버튼
     {
-        _gameStateText.text = _gameStateMessages[2]; // "�Ͻ�����" ���
+        _gameStateText.text = _gameStateMessages[2]; // 일시 정지 문구 출력
         _gameStateUICanvas.SetActive(true);
-        Time.timeScale = 0f; // ���� ����
+        Time.timeScale = 0f; // 게임 정지
     }
 
     #endregion
